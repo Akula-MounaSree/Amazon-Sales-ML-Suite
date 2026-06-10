3419eb439acf537fdbc3c19566b5ae17c5e44d2f
Akula-MounaSree <mounaakula@gmail.com>
2026-06-11 00:04:29 +0530

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly.express as px
 st.title("Sales Dashboard")
 st.image("https://eu-central-1-enterprise-euc1.graphassets.com/AAg7wOEyuSfq2w8Bzgd1Xz/qDzTq7U6SfeMqXMaTy9I")
-df=pd.read_csv("Amazon Sale Report.csv", low_memory=False)
+df=pd.read_csv("Amazon Sale Report.csv.gz", low_memory=False)
 df['Amount']=pd.to_numeric(df['Amount'], errors='coerce')
 st.subheader("Sales by Category")
 st.metric("Total Orders", len(df))
